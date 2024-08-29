05eedf7dfa6b8f8aaabb6c75a098f5cab8ad43ec
soumanso <soumanso@IoT.myguest.virtualbox.org>
2024-08-29 14:37:16 +0200

    Part2 done

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -8,5 +8,7 @@ export INSTALL_K3S_EXEC="server --node-ip=$1 --bind-address=$1 --flannel-iface=e
 
 curl -sfL https://get.k3s.io | sh -
 
-kubectl apply -n kube-system -f /vagrant_shared/app1.yaml --validate=warn
-kubectl apply -n kube-system -f /vagrant_shared/ingress.yaml --validate=warn
+kubectl apply -f /vagrant_shared/app1.yaml --validate=warn
+kubectl apply -f /vagrant_shared/app2.yaml --validate=warn
+kubectl apply -f /vagrant_shared/app3.yaml --validate=warn
+kubectl apply -f /vagrant_shared/ingress.yaml --validate=warn
