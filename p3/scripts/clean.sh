cec9d5b12abd1f1d99c1475dea4db49b42be359b
soumanso <soumanso@IoT.myguest.virtualbox.org>
2024-11-18 19:14:21 +0100

    WIP

--- p3/scripts/clean.sh
+++ p3/scripts/clean.sh
@@ -2,4 +2,12 @@
 
 sudo rm ./argocd_password.txt
 
+sudo kubectl delete deployments -n dev --all
+sudo kubectl delete services -n dev --all
+sudo kubectl delete pods -n dev --all
+
+sudo kubectl delete deployments -n argocd --all
+sudo kubectl delete services -n argocd --all
+sudo kubectl delete pods -n argocd --all
+
 sudo k3d cluster delete p3
--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -18,6 +18,3 @@ sudo kubectl get pods -n argocd
 echo Waiting for app pods to be ready...
 sudo kubectl wait pods -n dev --all --for condition=Ready --timeout=600s
 sudo kubectl get pods -n dev
-
-sudo kubectl port-forward -n argocd service/argocd-server 9090:443 &>/dev/null &
-sudo kubectl port-forward -n dev service/app-service 8888:8888 &>/dev/null &
